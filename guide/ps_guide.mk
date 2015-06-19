f4e461201e55671cad87192fb973c415d2c84e06
muli <muli@cs.cmu.edu>
2015-06-18 22:56:48 -0400

    add libdmlc.a in guide.mk

--- guide/ps_guide.mk
+++ guide/ps_guide.mk
@@ -1,6 +1,6 @@
 guide: guide/example_a guide/example_b guide/example_c guide/example_d guide/example_e
 
-LDFLAGS = $(EXTRA_LDFLAGS) $(PS_LDFLAGS) -lpthread # -lrt
+LDFLAGS = $(EXTRA_LDFLAGS) $(PS_LDFLAGS) $(CORE_PATH)/libdmlc.a -lpthread # -lrt
 
 guide/%: guide/%.cc $(PS_LIB)
 	$(CC) $(CFLAGS) $^ $(LDFLAGS) -o $@
