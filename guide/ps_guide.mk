6a88c222d196f56e8d9fb9418aa3c313e2d185c0
muli <muli@cs.cmu.edu>
2015-07-19 21:48:15 +0000

    add -rt in ps_guide.mk for linux

--- guide/ps_guide.mk
+++ guide/ps_guide.mk
@@ -1,6 +1,12 @@
 guide: $(addprefix guide/example_, a b c d e) #guide/network_perf # c d e
 
-LDFLAGS = $(EXTRA_LDFLAGS) $(PS_LDFLAGS) -lpthread # -lrt
+
+LDFLAGS = $(EXTRA_LDFLAGS) $(PS_LDFLAGS) -lpthread
+
+OS := $(shell uname -s)
+ifneq ($(OS), Darwin)
+    LDFLAGS += -lrt
+endif
 
 guide/%: guide/%.cc $(PS_LIB) $(PS_MAIN)
 	$(CXX) $(CFLAGS) $^ $(LDFLAGS) -o $@
