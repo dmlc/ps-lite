dd11e95e8f9bb57b3d20319f15d53e3515063706
muli <muli@cs.cmu.edu>
2015-07-19 20:49:08 +0000

    use static link in default

--- make/ps.mk
+++ make/ps.mk
@@ -11,8 +11,8 @@ ifeq ($(USE_KEY32), 1)
 PS_CFLAGS += -DUSE_KEY32=1
 endif
 
-ifeq ($(STATIC_DEPS), 1)
-PS_LDFLAGS += $(addprefix $(DEPS_PATH)/lib/, libprotobuf.a libglog.a libgflags.a libzmq.a libcityhash.a liblz4.a) # -lgssapi_krb5
-else
+ifeq ($(STATIC_DEPS), 0)
 PS_LDFLAGS += -L$(DEPS_PATH)/lib -lglog -lprotobuf -lgflags -lzmq -lcityhash -llz4
+else
+PS_LDFLAGS += $(addprefix $(DEPS_PATH)/lib/, libprotobuf.a libglog.a libgflags.a libzmq.a libcityhash.a liblz4.a) #-lgssapi_krb5
 endif
