704e9c144341295f7a81b36886b60fd20032cde8
muli <muli@cs.cmu.edu>
2015-08-03 16:42:25 -0400

    fix doc/setup.sh

--- doc/setup.sh
+++ doc/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+doxygen
+
 # put doxygen doc online
 
 rm html.tar.gz
@@ -8,7 +10,7 @@ tar -zcf html.tar.gz html
 host=linux.gp.cs.cmu.edu
 scp html.tar.gz $host:~
 
-ssh $host bash -c <<EOF
+ssh $host 'bash -s' <<EOF
 rm -rf www/ps-lite html
 tar -zxf html.tar.gz
 mkdir www/ps-lite
