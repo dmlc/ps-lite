95b5b6a2da2b475a7c88bdb35a8f800831f92e46
muli <muli@cs.cmu.edu>
2015-10-04 20:34:54 -0400

    add another pull in kv_cache to allow preallocated data

--- src/kv/kv_cache.h
+++ src/kv/kv_cache.h
@@ -29,48 +29,35 @@ class KVCache : public Customer {
     return Submit(&msg);
   }
 
+  /**
+   * vals should be pre-allocated with length len_vals. if you don't know the
+   * length at begining, use the next Pull
+   *
+   * if (vals_size != NULL) then use dynamic value length, otherwise it should
+   * be preallocated with length keys.size().
+   *
+   */
   inline int Pull(const Task& req, const SArray<K>& keys,
-                  std::vector<V>* vals, std::vector<int>* vals_size,
-                  const Message::Callback& cb) {
-    CHECK(IsKeysOrderd(keys)) << "keys must in non-decreasing order";
-    Message msg(req, kServerGroup);
-    mu_.lock();
-    int chl = chl_ ++;
-    auto& kv = pull_data_[chl];
-    mu_.unlock();
-    kv.key = keys;
-    kv.val = CHECK_NOTNULL(vals);
-    bool dyn_val = false;
-    if (vals_size) {
-      dyn_val = true;
-      msg.task.mutable_param()->set_dyn_val_size(true);
-      kv.val_size = vals_size;
+                  const Message::Callback& cb,
+                  V* vals, size_t len_vals,
+                  int* vals_size) {
+    return Pull_(req, keys, cb, vals, len_vals, NULL, vals_size);
+  }
+
+  /**
+   * if (vals_size != NULL) then use dynamic value length
+   * both vals and vals_size can be empty, we will allocate the memory
+   */
+  inline int Pull(const Task& req, const SArray<K>& keys,
+                  const Message::Callback& cb,
+                  std::vector<V>* vals, std::vector<int>* vals_size) {
+
+    if (vals_size && vals_size->empty()) {
+      vals_size->resize(keys.size());
     }
-    // LL << ts << " " << pull_data_[ts].key << " " << kv.value;
-    msg.callback = [chl, cb, dyn_val, this] {
-      if (dyn_val) {
-        // do a double check
-        mu_.lock();
-        auto& kv = pull_data_[chl];
-        mu_.unlock();
-        if (kv.matched_num != kv.key.size()) {
-          LOG(WARNING) << "unmatched " << kv.matched_num << " vs " << kv.key.size();
-        }
-        if (kv.val->size()) {
-          size_t len = 0;
-          for (int i : *(kv.val_size)) len += i;
-          CHECK_EQ(len, kv.val->size());
-        }
-      }
-      if (cb) cb();
-      mu_.lock();
-      pull_data_.erase(chl);
-      mu_.unlock();
-    };
-    msg.set_key(keys);
-    msg.task.set_key_channel(chl);
-    msg.task.mutable_param()->set_push(false);
-    return Submit(&msg);
+
+    return Pull_(req, keys, cb, NULL, 0, vals,
+                 (vals_size == NULL ? NULL : vals_size->data()));
   }
 
   /// called by system ///
@@ -89,15 +76,15 @@ class KVCache : public Customer {
     auto& kv = pull_data_[msg->task.key_channel()];
     mu_.unlock();
 
-
     if (msg->task.param().dyn_val_size()) {
       kv.recv_num ++;
       SArray<K> recv_key(msg->key);
       if (recv_key.size()) {
         CHECK_EQ(msg->value.size(), (size_t)2);
         SArray<int> recv_size(msg->value[1]);
+        SArray<int> val_size(kv.val_size, kv.key.size(), EmptyDel<int>());
         size_t n = ParallelOrderedMatch(
-            recv_key, recv_size, kv.key, kv.val_size, 1, AsOp::ASSIGN);
+            recv_key, recv_size, kv.key, &val_size, 1, AsOp::ASSIGN);
         CHECK_EQ(n, recv_size.size());
         kv.matched_num += n;
         kv.recv.push_back(std::make_pair(recv_key[0], SArray<V>(msg->value[0])));
@@ -112,8 +99,14 @@ class KVCache : public Customer {
                 });
       size_t len = 0;
       for (const auto& l : kv.recv) len += l.second.size();
-      kv.val->resize(len);
-      V* ptr = kv.val->data();
+      V* ptr = NULL;
+      if (kv.val_vec) {
+        kv.val_vec->resize(len);
+        ptr = kv.val_vec->data();
+      } else {
+        CHECK_EQ(kv.len_val, len);
+        ptr = kv.val;
+      }
       for (const auto& l : kv.recv) {
         memcpy(ptr, l.second.data(), l.second.size() * sizeof(V));
         ptr += l.second.size();
@@ -126,14 +119,73 @@ class KVCache : public Customer {
       CHECK_EQ(msg->value.size(), (size_t)1);
       SArray<V> recv_data(msg->value[0]);
       int k = recv_data.size() / recv_key.size();
-      size_t n = ParallelOrderedMatch(
-          recv_key, recv_data, kv.key, kv.val, k, AsOp::ASSIGN);
+
+      size_t n = 0;
+      if (kv.val_vec) {
+        n = ParallelOrderedMatch(
+            recv_key, recv_data, kv.key, kv.val_vec, k, AsOp::ASSIGN);
+      } else {
+        CHECK_EQ(kv.key.size() * k, kv.len_val);
+        SArray<V> my_val(kv.val, kv.len_val, EmptyDel<V>());
+        n = ParallelOrderedMatch(
+            recv_key, recv_data, kv.key, &my_val, k, AsOp::ASSIGN);
+      }
       CHECK_EQ(n, recv_data.size());
     }
-
   }
 
  private:
+  inline int Pull_(const Task& req, const SArray<K>& keys,
+                   const Message::Callback& cb,
+                   V* vals, size_t len_vals, std::vector<V>* vals_vec,
+                   int* vals_size) {
+    CHECK(IsKeysOrderd(keys)) << "keys must in non-decreasing order";
+    Message msg(req, kServerGroup);
+
+    mu_.lock();
+    int chl = chl_ ++;
+    auto& kv = pull_data_[chl];
+    mu_.unlock();
+    kv.key = keys;
+    kv.val = CHECK_NOTNULL(vals);
+    kv.len_val = len_vals;
+    kv.val_vec = vals_vec;
+    bool dyn_val = false;
+    if (vals_size) {
+      dyn_val = true;
+      msg.task.mutable_param()->set_dyn_val_size(true);
+      kv.val_size = vals_size;
+    }
+    // LL << ts << " " << pull_data_[ts].key << " " << kv.value;
+    msg.callback = [chl, cb, dyn_val, this] {
+      if (dyn_val) {
+        // do a double check
+        mu_.lock();
+        auto& kv = pull_data_[chl];
+        mu_.unlock();
+        if (kv.matched_num != kv.key.size()) {
+          LOG(WARNING) << "unmatched " << kv.matched_num << " vs " << kv.key.size();
+        }
+        CHECK(kv.val_size != NULL);
+        size_t len = 0;
+        for (size_t i = 0; i < kv.key.size(); ++i) len += kv.val_size[i];
+        if (kv.val_vec) {
+          CHECK_EQ(kv.val_vec->size(), len);
+        } else {
+          CHECK_EQ(kv.len_val, len);
+        }
+      }
+      if (cb) cb();
+      mu_.lock();
+      pull_data_.erase(chl);
+      mu_.unlock();
+    };
+    msg.set_key(keys);
+    msg.task.set_key_channel(chl);
+    msg.task.mutable_param()->set_push(false);
+    return Submit(&msg);
+  }
+
   inline bool IsKeysOrderd(const SArray<K>& keys) {
     for (size_t i = 0; i < keys.size() -1 ; ++i) {
       if (keys[i+1] < keys[i]) { return false; }
@@ -148,14 +200,16 @@ class KVCache : public Customer {
     //   [val_00, ..., val_0k, ..., val_n0, ..., val_nk]
     // dynamic value size:
     //   [val_00, ...val_0,val_size[0], ..., val_n0, ..., val_n,val_size[n]
-    std::vector<V>* val;
-    std::vector<int>* val_size = NULL;
+    V* val;
+    size_t len_val;  // length of val
+    std::vector<V>* val_vec = NULL;  // allocator fo val
+
+    int* val_size = NULL;
 
     // for match dynamic vals
     std::vector<std::pair<K, SArray<V>>> recv;
 
     int recv_num = 0;
-    // std::vector<K> recv_key;
     size_t matched_num = 0;
   };
 
