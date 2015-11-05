18d1656336ff9ad6bfa2dd4a391d3448fb7384f3
muli <muli@cs.cmu.edu>
2015-11-05 21:35:00 +0000

    remove -glog

--- make/ps.mk
+++ make/ps.mk
@@ -9,8 +9,8 @@ ifeq ($(USE_KEY32), 1)
 ADD_CFLAGS += -DUSE_KEY32=1
 endif
 
-PS_LDFLAGS_SO = -L$(DEPS_PATH)/lib -lglog -lprotobuf -lzmq
-PS_LDFLAGS_A = $(addprefix $(DEPS_PATH)/lib/, libprotobuf.a libglog.a libzmq.a)
+PS_LDFLAGS_SO = -L$(DEPS_PATH)/lib -lprotobuf -lzmq
+PS_LDFLAGS_A = $(addprefix $(DEPS_PATH)/lib/, libprotobuf.a libzmq.a)
 
 # ifeq ($(STATIC_DEPS), 0)
 # ADD_LDFLAGS += $(PS_LDFLAGS_SO)
