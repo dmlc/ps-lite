a1c84f7de6c4ecb00885b065a51c42c6c8ad43c0
muli <muli@cs.cmu.edu>
2016-04-10 17:25:15 -0400

    Sets a zero linger period when close a socket

--- src/zmq_van.h
+++ src/zmq_van.h
@@ -39,10 +39,18 @@ class ZMQVan : public Van {
   }
 
   void Stop() override {
+    PS_VLOG(1) << my_node_.ShortDebugString() << " is stopping";
     Van::Stop();
     // close sockets
-    zmq_close(receiver_);
-    for (auto& it : senders_) zmq_close(it.second);
+    int linger = 0;
+    int rc = zmq_setsockopt(receiver_, ZMQ_LINGER, &linger, sizeof(linger));
+    CHECK(rc == 0 || errno == ETERM);
+    CHECK_EQ(zmq_close(receiver_), 0);
+    for (auto& it : senders_) {
+      int rc = zmq_setsockopt(it.second, ZMQ_LINGER, &linger, sizeof(linger));
+      CHECK(rc == 0 || errno == ETERM);
+      CHECK_EQ(zmq_close(it.second), 0);
+    }
     zmq_ctx_destroy(context_);
   }
 
