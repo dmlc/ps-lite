78b4d34919e7e5d25d0da1c7933c5b440c845a6d
Jencir Lee <jli05@icloud.com>
2017-02-07 15:37:36 +0000

    use $(WGET) throughout deps.mk

--- make/deps.mk
+++ make/deps.mk
@@ -32,7 +32,7 @@ ${LZ4}:
 	$(eval FILE=lz4-r129.tar.gz)
 	$(eval DIR=lz4-r129)
 	rm -rf $(FILE) $(DIR)
-	wget $(URL)/$(FILE) && tar -zxf $(FILE)
+	$(WGET) $(URL)/$(FILE) && tar -zxf $(FILE)
 	cd $(DIR) && $(MAKE) && PREFIX=$(DEPS_PATH) $(MAKE) install
 	rm -rf $(FILE) $(DIR)
 
@@ -42,7 +42,7 @@ ${CITYHASH}:
 	$(eval FILE=cityhash-1.1.1.tar.gz)
 	$(eval DIR=cityhash-1.1.1)
 	rm -rf $(FILE) $(DIR)
-	wget $(URL)/$(FILE) && tar -zxf $(FILE)
+	$(WGET) $(URL)/$(FILE) && tar -zxf $(FILE)
 	cd $(DIR) && ./configure -prefix=$(DEPS_PATH) --enable-sse4.2 && $(MAKE) CXXFLAGS="-g -O3 -msse4.2" && $(MAKE) install
 	rm -rf $(FILE) $(DIR)
 
@@ -52,7 +52,7 @@ ${CITYHASH}:
 # 	$(eval FILE=gflags-2.0-no-svn-files.tar.gz)
 # 	$(eval DIR=gflags-2.0)
 # 	rm -rf $(FILE) $(DIR)
-# 	wget $(URL)/$(FILE) && tar -zxf $(FILE)
+# 	$(WGET) $(URL)/$(FILE) && tar -zxf $(FILE)
 # 	cd $(DIR) && ./configure -prefix=$(DEPS_PATH) && $(MAKE) && $(MAKE) install
 # 	rm -rf $(FILE) $(DIR)
 # gflags: | ${DEPS_PATH}/include/google/gflags.h
@@ -62,7 +62,7 @@ ${CITYHASH}:
 # 	$(eval FILE=v0.3.4.tar.gz)
 # 	$(eval DIR=glog-0.3.4)
 # 	rm -rf $(FILE) $(DIR)
-# 	wget https://github.com/google/glog/archive/$(FILE) && tar -zxf $(FILE)
+# 	$(WGET) https://github.com/google/glog/archive/$(FILE) && tar -zxf $(FILE)
 # 	cd $(DIR) && ./configure -prefix=$(DEPS_PATH) --with-gflags=$(DEPS_PATH) && $(MAKE) && $(MAKE) install
 # 	rm -rf $(FILE) $(DIR)
 # glog: | ${DEPS_PATH}/include/glog/logging.h
