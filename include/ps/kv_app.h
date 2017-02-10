ad3bb5ee0f5b733d9d3e63ee430932437986bc7d
formath <jinpengliu@163.com>
2017-02-10 22:39:52 +0800

    explicit add sender id and check_eq between sender_id and zmq_identify

--- include/ps/kv_app.h
+++ include/ps/kv_app.h
@@ -392,6 +392,7 @@ void KVServer<Val>::Response(const KVMeta& req, const KVPairs<Val>& res) {
   msg.meta.head        = req.cmd;
   msg.meta.timestamp   = req.timestamp;
   msg.meta.recver      = req.sender;
+  msg.meta.sender = Postoffice::Get()->van()->my_node().id;
   if (res.keys.size()) {
     msg.AddData(res.keys);
     msg.AddData(res.vals);
@@ -485,6 +486,7 @@ void KVWorker<Val>::Send(int timestamp, bool push, int cmd, const KVPairs<Val>& 
     msg.meta.head        = cmd;
     msg.meta.timestamp   = timestamp;
     msg.meta.recver      = Postoffice::Get()->ServerRankToID(i);
+    msg.meta.sender = Postoffice::Get()->van()->my_node().id;
     const auto& kvs = s.second;
     if (kvs.keys.size()) {
       msg.AddData(kvs.keys);
@@ -545,7 +547,7 @@ template <typename C, typename D>
 int KVWorker<Val>::Pull_(
     const SArray<Key>& keys, C* vals, D* lens, int cmd, const Callback& cb) {
   int ts = obj_->NewRequest(kServerGroup);
-  AddCallback(ts, [this, ts, keys, vals, lens, cb]() mutable {
+  AddCallback(ts, [this, ts, keys, vals, lens, cb]() mutable {  
       mu_.lock();
       auto& kvs = recv_kvs_[ts];
       mu_.unlock();
