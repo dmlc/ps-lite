be0d918d240a1d1a583dbd39ea1280c044d0bcc0
CodingCat <zhunansjtu@gmail.com>
2017-12-24 09:42:00 -0800

    recover other tests

--- tests/travis/travis_script.sh
+++ tests/travis/travis_script.sh
@@ -13,11 +13,11 @@ if [ ${TASK} == "test" ]; then
     make test DEPS_PATH=${CACHE_PREFIX} CXX=${CXX} || exit -1
     cd tests
     # single-worker tests
-    # tests=( test_connection test_kv_app test_simple_app )
-    # for test in "${tests[@]}"
-    # do
-    #    find $test -type f -executable -exec ./repeat.sh 4 ./local.sh 2 2 ./{} \;
-    # done
+    tests=( test_connection test_kv_app test_simple_app )
+    for test in "${tests[@]}"
+    do
+        find $test -type f -executable -exec ./repeat.sh 4 ./local.sh 2 2 ./{} \;
+    done
     # multi-workers test
     multi_workers_tests=( test_kv_app_multi_workers )
     for test in "${multi_workers_tests[@]}"
