0b07c403989d79606877c0e19693306a947b994a
crazyboycjr <chenjingrong@bytedance.com>
2018-04-28 19:05:17 +0800

    fix bug: data not aligned

--- src/rdma_van.h
+++ src/rdma_van.h
@@ -93,7 +93,8 @@ struct rdma_write_header {
   int sender;
   int recver;
   int length[5];
-};
+  int self_size;
+} __attribute__ ((packed));
 
 struct rdma_msg {
   /* rdma_msg_type */
@@ -280,7 +281,7 @@ class RDMAVan : public Van {
       conn->send_msg->data.length[i + 1] = msg.data[i].size();
     conn->send_msg->data.length[msg.data.size() + 1] = -1;
 
-    const size_t header_size = sizeof(struct rdma_write_header);
+    size_t header_size = (sizeof(struct rdma_write_header) + meta_size + 7) / 8 * 8 - meta_size;
     if (header_size + send_bytes <= kInlineData) {
       /* use send and recv region to transfer data */
       conn->send_msg->type |= MSG_INLINE_DATA;
@@ -292,11 +293,12 @@ class RDMAVan : public Van {
       header->sender = my_node_.id;
       header->recver = recver_id;
       header->length[0] = meta_size;
+      header->self_size = header_size;
       for (size_t i = 0; i < msg.data.size(); i++)
         header->length[i + 1] = msg.data[i].size();
       header->length[msg.data.size() + 1] = -1;
       /* fill meta */
-      char *addr = (char *)header + sizeof(struct rdma_write_header);
+      char *addr = (char *)header + header_size;
       meta.SerializeToArray(addr, meta_size);
       /* fill data */
       addr += meta_size;
@@ -353,6 +355,7 @@ class RDMAVan : public Van {
     header->sender = my_node_.id;
     header->recver = recver_id;
     header->length[0] = meta_size;
+    header->self_size = header_size;
 
     inspect(srmem.data(), srmem.size());
 
@@ -519,7 +522,7 @@ class RDMAVan : public Van {
         conn->send_msg->type = MSG_RES_REGION;
         auto &data = conn->recv_msg->data;
 
-        int total_length = sizeof(struct rdma_write_header);
+        int total_length = sizeof(struct rdma_write_header) + 8;
         for (int i = 0, length; (length = data.length[i]) != -1; i++) {
           total_length += length;
         }
@@ -575,7 +578,7 @@ class RDMAVan : public Van {
     msg->meta.sender = header->sender;
     msg->meta.recver = my_node_.id;
 
-    addr = static_cast<char *>(addr) + sizeof(*header);
+    addr = static_cast<char *>(addr) + header->self_size;
     UnpackMeta(static_cast<char *>(addr), header->length[0], &msg->meta);
 
     recv_bytes += header->length[0];
